2718c00733c89572ac1fa6f8ab5e223701fe7b19
shwarzes89 <shwarzes89@gmail.com>
2015-02-09 15:43:49 +0900

    add actionbar sherlock

--- BadUIApp/src/selab/dev/baduiapp/BadUIApp.java
+++ BadUIApp/src/selab/dev/baduiapp/BadUIApp.java
@@ -5,12 +5,12 @@ import android.content.Context;
 
 public class BadUIApp extends Application {
 	private static Context context;
-	
+
 	@Override
 	public void onCreate() {
 		context = this;
 	}
-	
+
 	public static Context getContext() {
 		return context;
 	}
