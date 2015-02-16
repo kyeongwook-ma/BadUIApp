6c8ec02b10275a3b379550668ae79ded194f0bda
Kyeong Wook Ma <shwarzes89@gmail.com>
2015-02-16 15:42:12 +0900

    implementd

--- BadUIApp/trace_parser.py
+++ BadUIApp/trace_parser.py
@@ -1,15 +1,20 @@
 # -*- coding:utf-8 -*-
 import sys, os, json, subprocess, re, signal
 
-def get_lines(file_path):
-    return list(open(file_path).read().splitlines())
+
+def write_bm(conn, bm_dict):
+
+    bm_cur = ((bm_dict["touch_class"], bm_dict["touch_mode"]))
+    conn.execute("INSERT INTO BMTable (touch_class, touch_mode) values (?, ?)", bm_cur)
+    conn.commit()
+
 
 def parse(line, keyword):
     fragments = line.split("\t")
 
     try:
         if keyword in fragments[1]:
-            return { "file_name" : fragments[4], "method_name" : fragments[2]}
+            return { "touch_class" : fragments[4], "touch_mode" : fragments[2]}
     except IndexError:
         pass
 
@@ -25,26 +30,36 @@ def get_pid(keyword):
         if keyword in ps:
             return extract_pid(ps)
 
+def start_recording(pid):
+    subprocess.Popen("adb shell am start " + pid + " /sdcard/method.trace")
+
+def stop_recording(pid):
+    subprocess.Popen("adb shell am stop " + pid)
+
+def pull_trace():
+    subprocess.Popen("adb pull /sdcard/method.trace " + os.getcwd())
+
 def main():
     
-    if len(sys.argv) < 3:
-        print('Usage : python trace_parser.py "tracefile path" "keyword" result.txt ')
+    if len(sys.argv) < 2:
+        print('Usage : python trace_parser.py "keyword" result.txt ')
         sys.exit(1)
 
-    lines = get_lines(sys.argv[1])
     out = open(sys.argv[3], "w")
     
     pid = get_pid()
+    start_recording(pid)
     
-    subprocess.Popen("adb shell am start " + pid + " method.trace &") 
-    signal.signal(signal.SIGINT, wrapup_record)
+    signal.signal(signal.SIGINT, signal_hadler)
  
-    def wrapup_record(signal, frame):
+    def signal_hadler(signal, frame):
+        stop_recording(pid)
+        pull_trace()
         for line in lines[24:]:
             info_dict = parse(line, sys.argv[2])
 
             if info_dict is not None:
-                out.write(str(info_dict)+"\n")
+                write_bm(str(info_dict))
 
 if __name__ == "__main__":
     main()
