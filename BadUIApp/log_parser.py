b68ca5d694da36f8d2f4fd2e4b1c8fa9380e6cc5
shwarzes89 <shwarzes89@gmail.com>
2015-04-13 14:54:56 +0900

    Refactoring

--- BadUIApp/log_parser.py
+++ BadUIApp/log_parser.py
@@ -19,26 +19,25 @@ def write_bm(conn, bm_dict):
 
 file_name = "device_logs.txt"
 
-def find_device():
-
-    p = subprocess.Popen("adb shell getevent -l > " + file_name, shell=True)
-
-    device_logs = open(file_name, "r").readlines()
-
-    os.kill(p.pid, signal.SIGINT)
-
-    touch_device_line = ""
-
-    for idx, log in reversed(list(enumerate(device_logs))):
-        if "touchscreen" in log:
-            touch_device_line = device_logs[idx-1]
-            break
-
-    touch_device_line = touch_device_line.replace(" ", "").replace("\t", "")
-    device_num = touch_device_line[len(touch_device_line)-3:]
-    return device_num
-
 def generate_touch_log():
+    def find_device():
+        p = subprocess.popen("adb shell getevent -l > " + file_name, shell=true)
+
+        device_logs = open(file_name, "r").readlines()
+
+        os.kill(p.pid, signal.sigint)
+
+        touch_device_line = ""
+
+        for idx, log in reversed(list(enumerate(device_logs))):
+            if "touchscreen" in log:
+                touch_device_line = device_logs[idx-1]
+                break
+    
+        touch_device_line = touch_device_line.replace(" ", "").replace("\t", "")
+        device_num = touch_device_line[len(touch_device_line)-3:]
+        return device_num
+    
     device_num = find_device()
     subprocess.Popen("adb shell getevent | grep event" + str(device_num) + " > touch_logs.txt", shell=True)
 
